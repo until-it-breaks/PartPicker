b48d831d371dd4cf4fb17c3cdd8d9b8e6b7c2a8d
Jiekai Sun <jiekai.sun@studio.unibo.it>
2024-08-01 21:46:33 +0200

    Imported DAO

---
+++ src/main/java/it/unibo/application/data/DAOException.java
@@ -0,0 +1,18 @@
+package it.unibo.application.data;
+
+public final class DAOException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public DAOException(String message) {
+        super(message);
+    }
+
+    public DAOException(Throwable cause) {
+        super(cause);
+    }
+
+    public DAOException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
